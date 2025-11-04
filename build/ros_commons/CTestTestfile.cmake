933a1b779b83318ccca23e6f63d6897a86223a21
Vito Franzosi <vito.franzosi@gmail.com>
2025-11-04 20:26:54 +0000

    correção no arquivo package.xml do submodulo rosbot3_gazebo

---
+++ build/ros_commons/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/ROS2AMR/docker/docker_environment/rosbot3_simulation/src/ros_commons
+# Build directory: /home/user/ROS2AMR/docker/docker_environment/rosbot3_simulation/build/ros_commons
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
